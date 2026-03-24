b9f39546aebaaa7246684a9d31dc35b96e2f2446
ck <ck@localhost.localdomain>
2026-03-24 08:03:38 -0400

    test 14

--- check_status.sh
+++ check_status.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+echo
+echo "=== SERVICES"
 kubectl get services -n enterprise-app
+echo
+echo
+echo "=== PODS"
 kubectl get pods -n enterprise-app
+echo
+echo
+echo "=== CONFIG-MAP"
 kubectl get configmap -n enterprise-app
+echo
+echo
+echo "=== SVC"
 kubectl get svc -n enterprise-app
+echo
+echo
+echo "=== NAMESPACES"
 kubectl get namespaces
+echo
+echo
+echo "=== INGRESS"
 kubectl get ingress -n enterprise-app
