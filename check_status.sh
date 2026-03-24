ae918f503b0dfa4a46432aa415df4ddca76eed96
ck <ck@localhost.localdomain>
2026-03-24 07:56:07 -0400

    test 13

---
+++ check_status.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+kubectl get services -n enterprise-app
+kubectl get pods -n enterprise-app
+kubectl get configmap -n enterprise-app
+kubectl get svc -n enterprise-app
+kubectl get namespaces
+kubectl get ingress -n enterprise-app
